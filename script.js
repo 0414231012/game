01b61c04844b1b4eb27ba50201b333bbd39b23ab
Jose <0414231012@s.tusd1.org>
2026-04-06 18:34:00 +0000

    0.0.2

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function start(){
     console.log("Game starting!")
-    createCircle();
+    setTimeout(createCircle, 500, 1);
+    setTimeout(createCircle, 1000, 2);
+    setTimeout(createCircle, 1500, 3);
+
 }
 
-function createCircle(){
+function createCircle(num){
     //div = circle!
     let circle = document.createElement("div");
 
     //styles
-    circle.innerText = "1";;
+    circle.innerText = num;
     circle.style.width = "180px";
     circle.style.height = "180px";
     circle.style.backgroundColor = "Red";
@@ -25,6 +28,11 @@ function createCircle(){
 
     //add to page
     document.body.appendChild(circle);
+
+    //make it clickable
+    circle.addEventListener("click", checkForRemove);
+
+
 }
 
 function getRandomX() {
@@ -35,4 +43,11 @@ function getRandomX() {
 function getRandomY() {
     let randY = Math.random() * window.innerHeight;
     return randY;
+}
+
+//checks for right circle and removes 
+
+function checkForRemove(event) {
+    let circle = event.target;
+    console.log(circle.innerText)
 }
